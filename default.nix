6ffef27af08bfa534ab1050dc7c893f41ee24eb2
Henri Rosten <henri.rosten@unikie.com>
2023-02-03 14:18:21 +0200

    sbomnix: add default.nix and shell.nix
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

---
+++ default.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+{
+  pkgs ? import <nixpkgs> {},
+  pythonPackages ? pkgs.python3Packages
+}:
+
+pythonPackages.buildPythonPackage rec {
+  pname = "sbomnix";
+  version = "1.0.0";
+  format = "setuptools";
+
+  src = ./.;
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "use_scm_version=True," "version='${version}'," \
+      --replace "setup_requires=['setuptools_scm']," "setup_requires=[],"
+  '';
+
+  propagatedBuildInputs = [ 
+    pkgs.reuse
+    pythonPackages.numpy
+    pythonPackages.pandas
+    pythonPackages.colorlog
+    pythonPackages.tabulate
+    pythonPackages.wheel
+    pythonPackages.packageurl-python
+    pythonPackages.graphviz
+  ];
+
+  pythonImportsCheck = [ "sbomnix" ];
+}
---
+++ shell.nix
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+{
+  pkgs ? import <nixpkgs> {},
+  pythonPackages ? pkgs.python3Packages
+}:
+
+pkgs.mkShell {
+  pname = "sbomnix-dev";
+
+  buildInputs = [ 
+    pkgs.reuse
+    pythonPackages.numpy
+    pythonPackages.pandas
+    pythonPackages.colorlog
+    pythonPackages.tabulate
+    pythonPackages.wheel
+    pythonPackages.packageurl-python
+    pythonPackages.graphviz
+    pythonPackages.pycodestyle
+    pythonPackages.pylint
+    pythonPackages.black
+    pythonPackages.pytest
+    pythonPackages.jsonschema
+    pythonPackages.venvShellHook
+  ];
+  venvDir = "venv";
+  postShellHook = ''
+    make install-dev
+  '';
+}
