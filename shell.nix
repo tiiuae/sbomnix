tiiuae/sbomnix

commit 60c81a4de4249142c763b657bae6063b8500d6fc

vulnxscan: make grype and vulnix dependencies explicit
- Make grype and vulnix dependencies explicit
- Flake update
- Use pip in place of pip3, add devshell dependency to pip
- Up the version to 1.4.1

Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

shell.nix
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   pkgs ? import <nixpkgs> {},
-  pythonPackages ? pkgs.python3Packages
+  pythonPackages ? pkgs.python3Packages,
+  vulnix ? import ./scripts/vulnxscan/vulnix.nix { nixpkgs=pkgs.path; pkgs=pkgs; },
 }:
 
 pkgs.mkShell {
@@ -12,6 +13,8 @@ pkgs.mkShell {
   buildInputs = [ 
     pkgs.reuse
     pkgs.grype
+    vulnix
+    pythonPackages.pip
     pythonPackages.numpy
     pythonPackages.pandas
     pythonPackages.colorlog
@@ -28,6 +31,10 @@ pkgs.mkShell {
     pythonPackages.venvShellHook
   ];
   venvDir = "venv";
+  # https://github.com/NixOS/nix/issues/1009:
+  shellHook = ''
+    export TMPDIR="/tmp"
+  '';
   postShellHook = ''
     make install-dev
   '';

.travis.yml
@@ -17,7 +17,7 @@ install:
   - source /etc/profile.d/nix.sh
   - nix-build '<nixpkgs>' -A hello
   # Install python requirements:
-  - pip3 install -r requirements.txt
+  - pip install -r requirements.txt
 script:
   # Ensure 'test-ci' targets pass:
   - make test-ci

Makefile
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 SHELL := bash
-PYTHON_TARGETS := $(shell find . -path ./venv -prune -o -path "*.eggs" -prune -o -name "*.py")
+PYTHON_TARGETS := $(shell find . -name "*.py" ! -path "*venv*" ! -path "*eggs*")
 
 define target_success
 	@printf "\033[32m==> Target \"$(1)\" passed\033[0m\n\n"
@@ -25,22 +25,22 @@ help: ## Show this help message
 	@grep -E '^[a-zA-Z_-]+:.*?##.*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}'
 
 install: ## Install sbomnix
-	pip3 install --user .
+	pip install --user .
 	$(call try_run_sbomnix,$@)
 	$(call target_success,$@)
 
 install-dev: uninstall install-dev-requirements ## Install for development
-	pip3 install --editable .
+	pip install --editable .
 	$(call try_run_sbomnix,$@)
 	$(call target_success,$@)
 
 uninstall: ## Uninstall sbomnix
 	find . -name '*.egg-info' -exec rm -fr {} +
-	pip3 uninstall -y sbomnix 
+	pip uninstall -y sbomnix 
 	$(call target_success,$@)
 
 install-dev-requirements: clean ## Install all requirements
-	pip3 install -q -r requirements.txt --no-cache-dir
+	pip install -q -r requirements.txt --no-cache-dir
 	$(call target_success,$@)
 
 pre-push: test black style pylint reuse-lint  ## Run tests, pycodestyle, pylint, reuse-lint

default.nix
@@ -3,12 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   pkgs ? import <nixpkgs> {},
-  pythonPackages ? pkgs.python3Packages
+  pythonPackages ? pkgs.python3Packages,
+  vulnix ? import ./scripts/vulnxscan/vulnix.nix { nixpkgs=pkgs.path; pkgs=pkgs; },
 }:
 
 pythonPackages.buildPythonPackage rec {
   pname = "sbomnix";
-  version = "1.3.0";
+  version = "1.4.1";
   format = "setuptools";
 
   src = ./.;
@@ -22,6 +23,7 @@ pythonPackages.buildPythonPackage rec {
   propagatedBuildInputs = [ 
     pkgs.reuse
     pkgs.grype
+    vulnix
     pythonPackages.numpy
     pythonPackages.pandas
     pythonPackages.colorlog

scripts/vulnxscan/vulnix.nix
@@ -1,0 +1,27 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+{ nixpkgs ? <nixpkgs>
+, pkgs ? import nixpkgs {}
+, lib ? pkgs.lib
+}:
+
+# Use build in upstream nixpkgs
+(pkgs.callPackage "${nixpkgs}/pkgs/tools/security/vulnix" {
+  python3Packages = pkgs.python3Packages;
+}).overrideAttrs (
+  old: rec {
+    # We use vulnix from 'https://github.com/henrirosten/vulnix' to get
+    # vulnix support for runtime-only scan ('-C' command-line option)
+    # which is currently not available in released version of vulnix.
+    src = pkgs.fetchFromGitHub {
+      owner = "henrirosten";
+      repo = "vulnix";
+      rev = "ad28b2924027a44a9b81493a0f9de1b0e8641005";
+      sha256 = "sha256-KXvmnaMjv//zd4aSwu4qmbon1Iyzdod6CPms7LIxeVU=";
+    };
+    version = "1.10.2.dev0";
+    name = "vulnix-${version}";
+  }
+)

scripts/vulnxscan/vulnxscan.py
@@ -16,6 +16,7 @@ import pathlib
 import json
 import re
 from tempfile import NamedTemporaryFile
+from shutil import which
 import pandas as pd
 import numpy as np
 from tabulate import tabulate
@@ -106,21 +107,10 @@ class VulnScan:
         """Run vulnix scan for nix artifact at target_path"""
         _LOG.info("Running vulnix scan")
         self.df_vulnix = pd.DataFrame()
-        # We use vulnix from 'https://github.com/henrirosten/vulnix' to get
-        # vulnix support for runtime-only scan ('-C' command-line option)
-        # which is currently not available in released version of vulnix.
-        # To manually test this on command-line, try something like:
-        # nix-shell \
-        #   --packages 'callPackage (fetchGit { url = URL; ref = "master"; } ) {}' \
-        #   --run "vulnix PATH_TO_TARGET -C --json"
-        url = "https://github.com/henrirosten/vulnix"
-        ref = '"master"'
-        packages = f"callPackage (fetchGit {{ url = {url}; ref = {ref}; }} ) {{}}"
         extra_opts = "-C --json"
         if buildtime:
             extra_opts = "--json"
-        run_vulnix = f"vulnix {target_path} {extra_opts}"
-        cmd = ["nix-shell", "--packages", packages, "--run", run_vulnix]
+        cmd = ["vulnix", target_path] + extra_opts.split()
         # vulnix exit status is non-zero if it found vulnerabilities,
         # therefore, we need to set the raise_on_error=False and
         # return_error=True to be able to read the command's stdout on
@@ -157,14 +147,7 @@ class VulnScan:
     def scan_grype(self, sbom_path):
         """Run grype scan using the SBOM at sbom_path as input"""
         _LOG.info("Running grype scan")
-        # To manually test this on command-line, try something like:
-        # nix-shell \
-        #   --packages grype \
-        #   --run "grype sbom:PATH_TO_SBOM --add-cpes-if-none --output json"
-        packages = "grype"
-        extra_opts = "--add-cpes-if-none --output json"
-        run_grype = f"grype sbom:{sbom_path} {extra_opts}"
-        cmd = ["nix-shell", "--packages", packages, "--run", run_grype]
+        cmd = ["grype", f"sbom:{sbom_path}", "--add-cpes-if-none", "--output", "json"]
         ret = exec_cmd(cmd)
         if ret:
             self._parse_grype(ret)
@@ -320,6 +303,14 @@ def _is_json(path):
         return False
 
 
+def exit_unless_command_exists(name):
+    """Check if `name` is an executable in PATH"""
+    name_is_in_path = which(name) is not None
+    if not name_is_in_path:
+        _LOG.fatal("command '%s' is not in PATH", name)
+        sys.exit(1)
+
+
 ################################################################################
 
 
@@ -330,9 +321,13 @@ def main():
     if not args.TARGET.exists():
         _LOG.fatal("Invalid path: '%s'", args.TARGET)
         sys.exit(1)
+
+    # Fail early if following commands are not in path
+    exit_unless_command_exists("grype")
+    exit_unless_command_exists("vulnix")
+
     target_path = args.TARGET.as_posix()
     target_path_abs = args.TARGET.resolve().as_posix()
-
     scanner = VulnScan()
     if args.sbom:
         if not _is_json(target_path_abs):
@@ -346,7 +341,6 @@ def main():
         sbom_path = _generate_sbom(target_path_abs, args.buildtime)
         _LOG.info("Using SBOM '%s'", sbom_path)
         scanner.scan_vulnix(target_path_abs, args.buildtime)
-
     scanner.scan_grype(sbom_path)
     scanner.scan_osv(sbom_path)
     scanner.report(args.out, target_path, args.buildtime, args.sbom)

commit e30e804007776223b5513828376951c11eff741c

sbomnix: fix nix dev shell
This commit fixes an issue that made the sbomnix nix development shell
not trigger the venvShellHook. This issue was introduced in commit:
60c81a4de4249142c763b657bae6063b8500d6fc.

Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

shell.nix
@@ -31,11 +31,11 @@ pkgs.mkShell {
     pythonPackages.venvShellHook
   ];
   venvDir = "venv";
-  # https://github.com/NixOS/nix/issues/1009:
-  shellHook = ''
-    export TMPDIR="/tmp"
-  '';
   postShellHook = ''
+    # https://github.com/NixOS/nix/issues/1009:
+    export TMPDIR="/tmp"
+    
+    # Enter python development environment
     make install-dev
   '';
 }