ab5e04d39f2358de71ed279bf6a8d490533760b5
Jonathan Ringer <jringer@anduril.com>
2024-09-30 13:10:04 -0700

    Fix metadata.properties depth value
    Signed-off-by: Jonathan Ringer <jringer@anduril.com>

--- src/sbomnix/sbomdb.py
+++ src/sbomnix/sbomdb.py
@@ -185,7 +185,7 @@ class SbomDb:
         if self.depth:
             prop = {}
             prop["name"] = "sbom_dependencies_depth"
-            prop["value"] = self.depth
+            prop["value"] = str(self.depth)
             cdx["metadata"]["properties"].append(prop)
         tool = {}
         tool["vendor"] = "TII"
