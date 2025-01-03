295f09f4b4ad0d46d7caa1dbe5e69615ca5d5b18
Henri Rosten <henri.rosten@unikie.com>
2025-01-03 13:00:00 +0200

    Apply nix fmt
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- nix/apps.nix
+++ nix/apps.nix
@@ -2,51 +2,55 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 {
-  perSystem = {self', ...}: {
-    apps = let
-      inherit (self'.packages) sbomnix;
-    in {
-      # nix run .#repology_cli
-      repology_cli = {
-        type = "app";
-        program = "${sbomnix}/bin/repology_cli";
-      };
+  perSystem =
+    { self', ... }:
+    {
+      apps =
+        let
+          inherit (self'.packages) sbomnix;
+        in
+        {
+          # nix run .#repology_cli
+          repology_cli = {
+            type = "app";
+            program = "${sbomnix}/bin/repology_cli";
+          };
 
-      # nix run .#repology_cve
-      repology_cve = {
-        type = "app";
-        program = "${sbomnix}/bin/repology_cve";
-      };
+          # nix run .#repology_cve
+          repology_cve = {
+            type = "app";
+            program = "${sbomnix}/bin/repology_cve";
+          };
 
-      # nix run .#nix_outdated
-      nix_outdated = {
-        type = "app";
-        program = "${sbomnix}/bin/nix_outdated";
-      };
+          # nix run .#nix_outdated
+          nix_outdated = {
+            type = "app";
+            program = "${sbomnix}/bin/nix_outdated";
+          };
 
-      # nix run .#nixgraph
-      nixgraph = {
-        type = "app";
-        program = "${sbomnix}/bin/nixgraph";
-      };
+          # nix run .#nixgraph
+          nixgraph = {
+            type = "app";
+            program = "${sbomnix}/bin/nixgraph";
+          };
 
-      # nix run .#nixmeta
-      nixmeta = {
-        type = "app";
-        program = "${sbomnix}/bin/nixmeta";
-      };
+          # nix run .#nixmeta
+          nixmeta = {
+            type = "app";
+            program = "${sbomnix}/bin/nixmeta";
+          };
 
-      # nix run .#vulnxscan
-      vulnxscan = {
-        type = "app";
-        program = "${sbomnix}/bin/vulnxscan";
-      };
+          # nix run .#vulnxscan
+          vulnxscan = {
+            type = "app";
+            program = "${sbomnix}/bin/vulnxscan";
+          };
 
-      # nix run .#provenance
-      provenance = {
-        type = "app";
-        program = "${sbomnix}/bin/provenance";
-      };
+          # nix run .#provenance
+          provenance = {
+            type = "app";
+            program = "${sbomnix}/bin/provenance";
+          };
+        };
     };
-  };
 }
--- nix/packages.nix
+++ nix/packages.nix
@@ -2,62 +2,66 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 {
-  perSystem = {
-    pkgs,
-    lib,
-    ...
-  }: let
-    pp = pkgs.python3Packages;
-    prefix_path = with pkgs; [
-      git
-      graphviz
-      grype
-      nix
-      nix-visualize
-      vulnix
-    ];
-  in {
-    packages = rec {
-      default = sbomnix;
+  perSystem =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      pp = pkgs.python3Packages;
+      prefix_path = with pkgs; [
+        git
+        graphviz
+        grype
+        nix
+        nix-visualize
+        vulnix
+      ];
+    in
+    {
+      packages = rec {
+        default = sbomnix;
 
-      sbomnix = pp.buildPythonPackage rec {
-        pname = "sbomnix";
-        version = pkgs.lib.removeSuffix "\n" (builtins.readFile ../VERSION);
-        format = "setuptools";
+        sbomnix = pp.buildPythonPackage rec {
+          pname = "sbomnix";
+          version = pkgs.lib.removeSuffix "\n" (builtins.readFile ../VERSION);
+          format = "setuptools";
 
-        src = lib.cleanSource ../.;
+          src = lib.cleanSource ../.;
 
-        nativeCheckInputs = with pp;
-          [
-            jsonschema
-            pytest
-          ]
-          ++ prefix_path;
+          nativeCheckInputs =
+            with pp;
+            [
+              jsonschema
+              pytest
+            ]
+            ++ prefix_path;
 
-        propagatedBuildInputs = with pp; [
-          beautifulsoup4
-          colorlog
-          dfdiskcache
-          filelock
-          graphviz
-          numpy
-          packageurl-python
-          packaging
-          pandas
-          pyrate-limiter
-          reuse
-          requests
-          requests-cache
-          requests-ratelimiter
-          tabulate
-        ];
+          propagatedBuildInputs = with pp; [
+            beautifulsoup4
+            colorlog
+            dfdiskcache
+            filelock
+            graphviz
+            numpy
+            packageurl-python
+            packaging
+            pandas
+            pyrate-limiter
+            reuse
+            requests
+            requests-cache
+            requests-ratelimiter
+            tabulate
+          ];
 
-        pythonImportsCheck = ["sbomnix"];
+          pythonImportsCheck = [ "sbomnix" ];
 
-        makeWrapperArgs = [
-          "--prefix PATH : ${lib.makeBinPath prefix_path}"
-        ];
+          makeWrapperArgs = [
+            "--prefix PATH : ${lib.makeBinPath prefix_path}"
+          ];
+        };
       };
     };
-  };
 }
