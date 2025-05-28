8c51d55be1b0df80d53dfce93f873783fa88f0b5
Henri Rosten <henri.rosten@unikie.com>
2025-05-28 11:16:58 +0300

    Move reuse lint from checks to pre-commit hooks
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- nix/checks.nix
+++ nix/checks.nix
@@ -16,18 +16,6 @@
     {
       checks =
         {
-          # checks that copyright headers are compliant
-          # todo this could be moved into a shared flake
-          reuse =
-            pkgs.runCommandLocal "reuse-lint"
-              {
-                nativeBuildInputs = [ pkgs.reuse ];
-              }
-              ''
-                cd ${self.outPath}
-                reuse lint
-                touch $out
-              '';
           pylint =
             pkgs.runCommandLocal "pylint"
               {
